31a85d87519df3b0d7f53fd387572b98947a90bd
Marcos Martins <marcos_36ma@hotmail.com>
2022-01-09 16:35:03 -0300

    Integração do fluxo de criação de usuários a base de dados

--- src/main/java/com/barbershop/business/UserBUImpl.java
+++ src/main/java/com/barbershop/business/UserBUImpl.java
@@ -1,14 +1,19 @@
 package com.barbershop.business;
 
 import com.barbershop.entity.User;
+import com.barbershop.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserBUImpl implements UserBU{
 
+    @Autowired
+    UserRepository userRepository;
+
     @Override
     public User createUser(User user) {
-        return user;
+        return userRepository.save(user);
     }
 
 }
--- src/main/java/com/barbershop/repository/UserRepository.java
+++ src/main/java/com/barbershop/repository/UserRepository.java
@@ -1,2 +1,10 @@
-package com.barbershop.repository;public class UserRepository {
+package com.barbershop.repository;
+
+import com.barbershop.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
 }
